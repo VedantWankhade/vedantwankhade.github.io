08facd473223d79659dfb5395994524244133605
VedantWankhade <vedantwankhade17@gmail.com>
2024-03-29 14:21:19 +0530

    Revert "Navbar blur and remove copyright"
    This reverts commit c09ea95ac2ed695b675674ece6171d0a1505dcdf.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,13 +33,13 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   };
 
   return <div className={`text-lg font-inter min-h-screen container flex flex-col justify-between max-w-full m-0 bg-ctp-base text-ctp-text ${theme}`}>
-          <header className="p-6 bg-ctp-base animate-slide-down sticky top-0 z-10 bg-opacity-30 backdrop-blur-lg backdrop-filter border border-ctp-base">
+          <header className="p-6 bg-ctp-mantle animate-slide-down sticky top-0 z-10">
             <Navbar cycleTheme={cycleTheme} theme={theme} toggleMenu={toggleMenu} menuIcon={menuIcon} slideMenuFlag={slideMenuFlag} />
           </header>
           {/* <div className="hidden sm:block">
             <p className="text-2xl font-maple"> you@github.io# ~ /<span>vedant</span><span>wankhade</span></p>
           </div> */}
-          <div className={`border border-ctp-base bg-opacity-30 backdrop-blur-lg backdrop-filter ${slideMenuFlag} z-10 bg-ctp-mantle animate-slide-left right-0 p-4 bottom-0 w-1/4 h-screen fixed translate-x-0`}>
+          <div className={`${slideMenuFlag} z-10 bg-ctp-mantle animate-slide-left right-0 p-4 bottom-0 w-1/4 h-screen fixed translate-x-0`}>
             <ul className="flex flex-col gap-4 p-1">
               <button><i onClick={() => toggleMenu(menuIcon, slideMenuFlag)} className={`ri-${menuIcon}-fill text-3xl`}></i></button>
                 <li><Link className="flex flex-col items-center" to="/"><i className={`ri-user-fill text-3xl`}></i><p className="text-sm">About</p></Link></li>
@@ -53,9 +53,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           <main className="py-6 px-6 lg:px-16 xl:px-80 flex justify-center sm:justify-between gap-6">
             { children }
           </main>
-          <footer className="p-6 bg-ctp-base flex justify-between">
-            {/* <p>&copy; 2024 Vedant Wankhade</p> */}
-            <p></p>
+          <footer className="p-6 bg-ctp-mantle flex justify-between">
+            <p>&copy; 2024 Vedant Wankhade</p>
             <a href="https://github.com/vedantwankhade/vedantwankhade.github.io" target="_blank"><i className={`ri-github-fill text-3xl`}></i></a>
           </footer>
         </div>
